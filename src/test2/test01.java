9652d986e760db1f563476c69e29e239ddcf558a
unknown <x15g022@PC505N016.chiba-fjb.ac.jp>
2015-09-11 11:51:16 +0900

    変更

--- src/test2/test01.java
+++ src/test2/test01.java
@@ -4,6 +4,9 @@ public class test01 {
 
 	public static void main(String[] args) {
 		System.out.println("ああわあ");
+		System.out.println("ああわあ");
+		System.out.println("ああわあ");
+		System.out.println("ああわあ");
 
 	}
 
